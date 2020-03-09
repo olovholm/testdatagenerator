4cdf870ada3fc056c9eb7e62ed631ef4014f81df
Ola Lovholm <ola.lovholm@soprasteria.com>
2020-03-09 21:26:43 +0100

    Beskrivelse på operasjon

--- src/main/kotlin/net/lovholm/testdata/controller/PersonIdentController.kt
+++ src/main/kotlin/net/lovholm/testdata/controller/PersonIdentController.kt
@@ -1,16 +1,13 @@
 package net.lovholm.testdata.controller
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.parameters.RequestBody
 import net.lovholm.testdata.domain.FoedselsnummerGenerator
 import net.lovholm.testdata.dto.GenererFødselsnummerRequestDto
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.util.MimeType
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 
 @RestController
@@ -20,7 +17,7 @@ class PersonIdentController {
     @RequestMapping(method = [RequestMethod.POST], path = ["/fodselsnummer"],
         consumes = arrayOf(MediaType.APPLICATION_JSON_VALUE),
         produces = arrayOf(MediaType.TEXT_PLAIN_VALUE) )
-    @Operation
+    @Operation(description = "Generer syntetiske FNR/DNR")
     fun genererFødselsnummer(@org.springframework.web.bind.annotation.RequestBody request: GenererFødselsnummerRequestDto) : ResponseEntity<String> {
         val generatedResult = FoedselsnummerGenerator(request.kjønn, request.identType, request.fodselsdato).generate()
         return ResponseEntity.ok(generatedResult)
